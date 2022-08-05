d0ba00125ba39eaeaceedd8ed2f1bfa444f6351d
릴리\User <jon00141@naver.com>
2022-08-05 21:03:58 +0900

    (김지현)  - post list jsp, api 구성  - jsp 파일 section 구성  필요

---
+++ src/main/java/com/memo/post/PostController.java
@@ -0,0 +1,17 @@
+package com.memo.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+	
+	// post 리스트 페이지
+	@GetMapping("/post/list/view")
+	public String postList() {
+		
+		return "post/list";
+		
+	}
+
+}
