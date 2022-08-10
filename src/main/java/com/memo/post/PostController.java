84a49f702ec71b3e5650646f27850e0c80b84aa2
릴리\User <jon00141@naver.com>
2022-08-11 00:36:29 +0900

    (김지현)  - post list 페이지 연결  - 메모 세부 페이지(detail) 연결

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -1,14 +1,37 @@
 package com.memo.post;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.memo.post.bo.PostBO;
+import com.memo.post.model.Post;
 
 @Controller
 public class PostController {
 	
+	@Autowired
+	private PostBO postBO;
+	
+	
 	// post 리스트 페이지
 	@GetMapping("/post/list/view")
-	public String postList() {
+	public String postList(HttpServletRequest request, Model model) {
+		
+		HttpSession session = request.getSession();
+		
+		// 로그인한 사용자의 userId
+		int userId = (Integer) session.getAttribute("userId");		// 다운 캐스팅
+		
+		List<Post> postList = postBO.getPostList(userId);
+		model.addAttribute("memoList", postList);
 		
 		return "post/list";
 	}
@@ -19,7 +42,20 @@ public class PostController {
 	public String postInput() {
 		
 		return "post/create";
-		
 	}
-
+	
+	
+	
+	// 메모 세부 페이지
+	// post의 id를 전달 받고 해당하는 메모를 조회하여 model에 추가
+	@GetMapping("/post/detail/view")
+	public String postDetail(@RequestParam("id") int id, Model model) {
+		
+		Post post = postBO.getPost(id);
+		
+		model.addAttribute("memo", post);
+		
+		return "post/detail";
+	}
+	
 }
