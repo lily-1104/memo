3f5655b6fc08e313f3062509ab9a104b768704a6
릴리\User <jon00141@naver.com>
2023-11-01 00:20:09 +0900

    게시글 리스트 화면 구성, 연결, API 구현

---
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -0,0 +1,27 @@
+package com.memo.post.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.post.domain.Post;
+import com.memo.post.mapper.PostMapper;
+
+@Service
+public class PostBO {
+	
+	
+	@Autowired
+	private PostMapper postMapper;
+	
+	
+	// 게시글 리스트
+	// input : userId / output : List<Post>
+	public List<Post> getPostListByUserId(int userId) {
+		
+		return postMapper.selectPostListByUserId(userId);
+		
+	}
+
+}
--- src/main/java/com/memo/post/mapper/PostMapper.java
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -5,9 +5,18 @@ import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
+import com.memo.post.domain.Post;
+
 @Repository
 public interface PostMapper {
 	
+	
+	// TestController
 	public List<Map<String, Object>> selectPostList();
-
+	
+	
+	
+	// 게시글 리스트
+	public List<Post> selectPostListByUserId(int userId);
+	
 }
