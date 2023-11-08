5ed68683e7755dfb58d7249af31fdd8cf36b29a0
릴리\User <jon00141@naver.com>
2023-11-08 21:39:10 +0900

    로그 추가, 이미지 삭제 API 구현

--- src/main/java/com/memo/common/FileManagerService.java
+++ src/main/java/com/memo/common/FileManagerService.java
@@ -6,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileManagerService {
 	
 	
+	private Logger logger = LoggerFactory.getLogger(FileManagerService.class);
+			
+			
 	// 실제 업로드가 된 이미지가 저장될 경로 (서버 주소)
 	public static final	String FILE_UPLOAD_PATH = "C:\\Users\\User\\Desktop\\개발\\5_Spring_Project 2023\\memo\\workspace\\images/";	
 			// static final : 상수, 한번 세팅 후 절대 안바꿀 때 사용, 보통 상수 뒤에는 대문자로 사용 (FILE_UPLOAD_PATH)
@@ -21,6 +26,7 @@ public class FileManagerService {
 	
 	
 	
+	// 이미지 업로드 => 저장
 	// input : userLoginId, file(이미지파일) / output : web imagePath
 	public String saveFile(String loginId, MultipartFile file) {
 		
@@ -50,7 +56,8 @@ public class FileManagerService {
 			Files.write(path, bytes);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
+			logger.error("[이미지 업로드] 업로드 실패 loginId:{}, filePath:{}", loginId, filePath);
 			e.printStackTrace();
 			return null;	// 이미지 업로드 실패 시 null로 리턴
 		}
@@ -62,5 +69,43 @@ public class FileManagerService {
 		return "/images/" + directoryName + "/" + file.getOriginalFilename();
 		
 	}
+	
+	
+	
+	// 이미지 삭제
+	// imput : imagePath / output : X
+	public void deleteFile(String imagePath) {	 // imagePath : /images/aaaaa_1698923969256/짱아코.jpg
+		
+		// C:\\Users\\User\\Desktop\\개발\\5_Spring_Project 2023\\memo\\workspace\\images/
+		// 위의 두 주소에 겹치는 /images/ 를 제거한다
+		Path path = Paths.get(FILE_UPLOAD_PATH + imagePath.replace("/images/", ""));	// /images/를 아무것도 없는 상태("")로 치환
+		
+		if (Files.exists(path)) {		// 이미지가 존재하는가?
+			
+			// 이미지 삭제
+//			Files.delete(path);		=> try/catch로 만들기
+			
+			try {
+				Files.delete(path);
+			} catch (IOException e) {
+				logger.error("[이미지 삭제] 파일 삭제 실패. imagePath:{}", imagePath);
+				return;
+			}
+			
+			
+			// 폴더(디렉토리) 삭제
+			path = path.getParent();
+			
+			if (Files.exists(path)) {
+				try {
+					Files.delete(path);
+				} catch (IOException e) {
+					logger.error("[이미지 삭제] 폴더 삭제 실패. imagePath:{}", imagePath);
+				}
+			}
+			
+		}
+	}
+	
 	
 }
